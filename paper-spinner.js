4fc958d508e56010e235351cfaaa92ad01d3a80b
Vivekbyri <vivek.byri@springml.com>
2020-02-07 12:00:23 +0530

    update package.json

--- paper-spinner.js
+++ paper-spinner.js
@@ -1,13 +1,3 @@
-/**
-@license
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at
-http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
-http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
-found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
-part of the polymer project is also subject to an additional IP rights grant
-found at http://polymer.github.io/PATENTS.txt
-*/
 // import '@polymer/polymer/polymer-legacy.js';
 // import '@polymer/paper-styles/color.js';
 // import './paper-spinner-styles.js';
@@ -17,42 +7,6 @@ import {html} from '@polymer/polymer/lib/utils/html-tag.js';
 
 import {PaperSpinnerBehavior} from './paper-spinner-behavior.js';
 
-/**
-Material design: [Progress &
-activity](https://www.google.com/design/spec/components/progress-activity.html)
-
-Element providing a multiple color material design circular spinner.
-
-    <paper-spinner active></paper-spinner>
-
-The default spinner cycles between four layers of colors; by default they are
-blue, red, yellow and green. It can be customized to cycle between four
-different colors. Use <paper-spinner-lite> for single color spinners.
-
-### Accessibility
-
-Alt attribute should be set to provide adequate context for accessibility. If
-not provided, it defaults to 'loading'. Empty alt can be provided to mark the
-element as decorative if alternative content is provided in another form (e.g. a
-text block following the spinner).
-
-    <paper-spinner alt="Loading contacts list" active></paper-spinner>
-
-### Styling
-
-The following custom properties and mixins are available for styling:
-
-Custom property | Description | Default
-----------------|-------------|----------
-`--paper-spinner-layer-1-color` | Color of the first spinner rotation | `--google-blue-500`
-`--paper-spinner-layer-2-color` | Color of the second spinner rotation | `--google-red-500`
-`--paper-spinner-layer-3-color` | Color of the third spinner rotation | `--google-yellow-500`
-`--paper-spinner-layer-4-color` | Color of the fourth spinner rotation | `--google-green-500`
-`--paper-spinner-stroke-width` | The width of the spinner stroke | 3px
-
-@element paper-spinner
-@demo demo/index.html
-*/
 Polymer({
   /** @override */
   _template: html`
